517e0975101a31f0365c54b8e5884e097219b108
gugunm <mediamergugun2@gmail.com>
2019-04-08 20:51:27 +0700

    ngerapihin main.py

--- App/Main.py
+++ App/Main.py
@@ -2,6 +2,21 @@ import Preprocessing as pr
 
 if __name__ == "__main__":
     data_path = 'data/input/*.csv'
+
+    # --- Load Preprocessing ---
+    print("=== Preprocessing ===")
     data = pr.preprocessing(data_path=data_path)
+
+    # --- Handle Ambiguitas B.Inggris (negasi) ---
+
+    # --- Cross Validation Using 10-Fold ---
+
+    # --- Feature Extraction TF-IDF ---
+
+    # --- Feature Selection C-Square ---
+
+    # --- Classification Using KNN ---
+
+    # --- Evaluation Recall, Precission, F-Measure ---
 
     print(data)
